3c42181b2291155ffc7229d60988f4b42bc00618
SIRIWAT DAMRONGTAMWATTANA <99265@icc.co.th>
2026-03-30 14:48:04 +0700

    Add CORS support and create initial HTML for books list

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express'
+import cors, {CorsOptions} from 'cors';
 import { getAllBooks, getBookByCategory, getBookById, addBook } from "./services/BookService";
 import bookRoutes from "./routes/BookRoute";
 import type  Book from "./models/Book";
@@ -7,6 +8,16 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
+
+const corsOptions: CorsOptions = {
+   origin: ['http://localhost:5051', 
+            'http://127.0.0.1:5500', 
+            'http://localhost:5500'],
+   methods: ['GET','POST','OPTIONS','PUT','DELETE'],
+   allowedHeaders: ['Content-Type','Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/books', bookRoutes);
 
